a394245d805d7e14ee509d159ab8ea37bc137620
ShirGoldfarb <shir@uniqai.com>
2023-06-20 12:08:27 +0300

    Change

--- main.py
+++ main.py
@@ -45,14 +45,11 @@ att_select, heatmap = st.columns(2)
 attributes = ['lead_time', 'arrival_date_year',
        'arrival_date_month', 'arrival_date_week_number',
        'arrival_date_day_of_month', 'stays_in_weekend_nights',
-       'stays_in_week_nights', 'adults', 'children', 'babies', 'meal',
-       'country', 'market_segment', 'distribution_channel',
+       'stays_in_week_nights', 'adults', 'children', 'babies',
        'is_repeated_guest', 'previous_cancellations',
-       'previous_bookings_not_canceled', 'reserved_room_type',
-       'assigned_room_type', 'booking_changes', 'deposit_type', 'agent',
-       'company', 'days_in_waiting_list', 'customer_type', 'adr',
-       'required_car_parking_spaces', 'total_of_special_requests',
-       'reservation_status', 'reservation_status_date']
+       'previous_bookings_not_canceled', 'booking_changes', 'agent',
+       'company', 'days_in_waiting_list', 'adr',
+       'required_car_parking_spaces', 'total_of_special_requests']
 with att_select:
     selected_attributes = st.multiselect(
             "Select Attributes",
