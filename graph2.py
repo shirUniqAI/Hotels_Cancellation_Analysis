2c11c64d50a7548466d3bbe646bee123f279e49c
ShirGoldfarb <shir@uniqai.com>
2023-06-20 17:11:05 +0300

    Change

--- graph2.py
+++ graph2.py
@@ -8,6 +8,8 @@ hotel_bookings = pd.read_csv("hotel_bookings.csv")
 
 def get_corr_heatmap(columns: list = None):
     df = hotel_bookings.copy()
+    if columns is None:
+        columns = df.columns
     if "is_canceled" not in columns:
         columns.insert(0, "is_canceled")
     if columns is not None and len(columns) > 1:
@@ -15,7 +17,8 @@ def get_corr_heatmap(columns: list = None):
     corr = df.corr(numeric_only=True)
     fig = px.imshow(corr,
                     title="Attributes Correlation Matrix",
-                    aspect="auto"
+                    aspect="auto",
+                    color_continuous_scale='RdBu_r'
                     )
     return fig
 
